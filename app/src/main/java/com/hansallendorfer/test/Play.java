4914a17e2da33ed56c57cbf67175fc8319deb768
Hans F. Allendorfer <hansallendorfer@gmail.com>
2015-02-28 19:21:37 -0500

    removed bottom 2 buttons, added hella sound buttons

--- app/src/main/java/com/hansallendorfer/test/Play.java
+++ app/src/main/java/com/hansallendorfer/test/Play.java
@@ -1,19 +1,14 @@
 package com.hansallendorfer.test;
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.media.MediaPlayer;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
-import java.util.Timer;
-import java.util.TimerTask;
+        import android.graphics.Color;
+        import android.media.MediaPlayer;
+        import android.support.v7.app.ActionBarActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.Spinner;
+        import java.util.Timer;
+        import java.util.TimerTask;
 
 
 public class Play extends ActionBarActivity {
@@ -74,23 +69,23 @@ public class Play extends ActionBarActivity {
         // viewpager.setAdapter(p);
         //hi ankit
         a1 = (Button) findViewById(R.id.a1);
-        a2 = (Button) findViewById(R.id.a2);
+        a2 = (Button) findViewById(R.id.a5);
         a3 = (Button) findViewById(R.id.a3);
-        a4 = (Button) findViewById(R.id.a4);
-        b1 = (Button) findViewById(R.id.b1);
-        b2 = (Button) findViewById(R.id.b2);
-        b3 = (Button) findViewById(R.id.b3);
-        b4 = (Button) findViewById(R.id.b4);
-        c1 = (Button) findViewById(R.id.c1);
-        c2 = (Button) findViewById(R.id.c2);
+        a4 = (Button) findViewById(R.id.c5);
+        b1 = (Button) findViewById(R.id.a2);
+        b2 = (Button) findViewById(R.id.a6);
+        b3 = (Button) findViewById(R.id.c2);
+        b4 = (Button) findViewById(R.id.c6);
+        c1 = (Button) findViewById(R.id.a3);
+        c2 = (Button) findViewById(R.id.a7);
         c3 = (Button) findViewById(R.id.c3);
-        c4 = (Button) findViewById(R.id.c4);
-        d1 = (Button) findViewById(R.id.d1);
-        d2 = (Button) findViewById(R.id.d2);
-        d3 = (Button) findViewById(R.id.d3);
+        c4 = (Button) findViewById(R.id.c7);
+        d1 = (Button) findViewById(R.id.c5);
+        d2 = (Button) findViewById(R.id.a8);
+        d3 = (Button) findViewById(R.id.c7);
         d4 = (Button) findViewById(R.id.d4);
 
-       final TimerTask beat1 = new TimerTask() {
+        final TimerTask beat1 = new TimerTask() {
             @Override
             public void run() {
                 if(a1_pressed){
@@ -108,7 +103,7 @@ public class Play extends ActionBarActivity {
             }
         };
 
-       final TimerTask beat2 = new TimerTask() {
+        final TimerTask beat2 = new TimerTask() {
             @Override
             public void run() {
                 if(b1_pressed){
@@ -144,7 +139,7 @@ public class Play extends ActionBarActivity {
             }
         };
 
-       final TimerTask beat4 = new TimerTask() {
+        final TimerTask beat4 = new TimerTask() {
             @Override
             public void run() {
                 if(d1_pressed){
@@ -215,7 +210,7 @@ public class Play extends ActionBarActivity {
         t.start();
     }
 
-        //make methods to play each sound individually
+    //make methods to play each sound individually
 
     public void playbeep1() {
         mp = MediaPlayer.create(Play.this, R.raw.clap);
