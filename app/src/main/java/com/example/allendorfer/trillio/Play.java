61588397b13014e182513a1783b258b32d8b2fa9
Hans Allendorfer <hansallendorfer@gmail.com>
2015-06-10 18:47:42 -0700

    2D Boolean ArrayList added addBar/removeBar methods added SoundPool and sounds added

--- app/src/main/java/com/example/allendorfer/trillio/Play.java
+++ app/src/main/java/com/example/allendorfer/trillio/Play.java
@@ -2,6 +2,8 @@ package com.example.allendorfer.trillio;
 
 import android.app.Activity;
 import android.graphics.PorterDuff;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,23 +13,47 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /* New age */
 
 public class Play extends Activity {
 
+    SoundPool sounds;
+
     Map<String, Boolean> map;
+    List<List<Boolean>> map2;
     Firebase firebase;
     Firebase board;
+    Firebase board2;
+    int numCols = -1;
 
     public void initialize(){
         map = new HashMap<>();
+        map2 = new ArrayList<List<Boolean>>();
+        for(int i = 0; i < 8; i++){
+            map2.add(new ArrayList<Boolean>());
+            numCols++;
+            for(int j = 0; j < 8; j++){
+                map2.get(i).add(false);
+            }
+        }
         for(int i = 1; i <= 64; i++){
             String b = "button" + i;
             map.put(b, false);
         }
+        sounds = new SoundPool(8, AudioManager.STREAM_MUSIC,0);
+        sounds.load(this, R.raw.a, 1);
+        sounds.load(this, R.raw.b, 1);
+        sounds.load(this, R.raw.c, 1);
+        sounds.load(this, R.raw.d, 1);
+        sounds.load(this,R.raw.e,1);
+        sounds.load(this, R.raw.f, 1);
+        sounds.load(this, R.raw.g, 1);
+        sounds.load(this, R.raw.h, 1);
     }
 
     @Override
@@ -39,7 +65,10 @@ public class Play extends Activity {
         Firebase.setAndroidContext(this);
         firebase = new Firebase("https://trillio.firebaseio.com/");
         board = firebase.child("board");
+        board2 = firebase.child("board2");
+
         board.setValue(map);
+        board2.setValue(map2);
 
         firebase.child("board").addChildEventListener(new ChildEventListener() {
             @Override
@@ -52,10 +81,10 @@ public class Play extends Activity {
                 int id = getResources().getIdentifier(key, "id", getPackageName());
                 Button button = (Button) findViewById(id);
 
-                if(dataSnapshot.getValue() == true){
+                if (dataSnapshot.getValue() == true) {
                     button.getBackground().setColorFilter(0xFFFF0000, PorterDuff.Mode.MULTIPLY);
                     map.put(dataSnapshot.getKey(), true);
-                }else{
+                } else {
                     button.getBackground().clearColorFilter();
                     map.put(dataSnapshot.getKey(), false);
                 }
@@ -88,6 +117,25 @@ public class Play extends Activity {
             board.child(buttonId).setValue(true);
        }
         board.setValue(map);
+    }
+
+    public void addBar(View view){
+        if(numCols < 15){
+            map2.add(new ArrayList<Boolean>());
+            numCols++;
+            for(int j = 0; j < 8; j++){
+                map2.get(numCols).add(false);
+            }
+            board2.setValue(map2);
+        }
+    }
+
+    public void removeBar(View view){
+        if(numCols != 0){
+            map2.remove(numCols);
+            numCols--;
+            board2.setValue(map2);
+        }
     }
 
 }
